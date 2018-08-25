04a4d62d4c13a710da1a04fb1f8cdd70f0586502
Sideesh <lbsideeshwaran@gmail.com>
2018-08-25 22:33:35 +0530

    Initial brand new

---
+++ Tozzle/ToDoListViewController.swift
@@ -0,0 +1,50 @@
+//
+//  ViewController.swift
+//  Tozzle
+//
+//  Created by Sideeshwaran L B on 25/08/18.
+//  Copyright © 2018 Sideeshwaran L B. All rights reserved.
+//
+
+import UIKit
+
+class ToDoListViewController: UITableViewController {
+    
+    let itemArray = ["Find Harry", "Buy Eggs", "Destroy Dragons"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    //MARK - TableView DataSource Methods
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        
+        cell.textLabel?.text = itemArray[indexPath.row]
+        return cell
+    }
+    
+    //MARK - TableView Delegate Methods
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(indexPath.row)
+        
+        
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark{
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        }else{
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+
+}
+
